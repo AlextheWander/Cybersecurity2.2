da5953efc5110225d4f6d4e78677f94ae9ee38f6
AlextheWander <90612455+AlextheWander@users.noreply.github.com>
2022-04-25 12:54:29 +0200

    QUIZZ 2
    quizz 2
    - mangler video setup
    - qiuzz 3-4-5-6
    - mere information om phising

--- app/main.js
+++ app/main.js
@@ -7,6 +7,8 @@ bars.addEventListener('click', function () {
 });
 
 
+// Spørgsmål 1
+
   // Functionen evaluere svaret og viser resultatet til brugeren alt efter om det er rigtigt eller forkert
   function displayAnswer1() {
     if (document.getElementById('option-11').checked) {
@@ -47,5 +49,49 @@ bars.addEventListener('click', function () {
       document.getElementById('result-11').style.color = 'limegreen'
       document.getElementById('result-11').innerHTML = 'Correct!'
       document.getElementById('showanswer1').removeChild(showAnswer1)
+    })
+  }
+
+  // Spørgsmål 2
+
+  function displayAnswer2() {
+    if (document.getElementById('option-15').checked) {
+      document.getElementById('block-15').style.border = '3px solid limegreen'
+      document.getElementById('result-15').style.color = 'limegreen'
+      document.getElementById('result-15').innerHTML = 'Incorrect!'
+    }
+    if (document.getElementById('option-16').checked) {
+      document.getElementById('block-16').style.border = '3px solid red'
+      document.getElementById('result-16').style.color = 'red'
+      document.getElementById('result-16').innerHTML = 'Incorrect!'
+      showCorrectAnswer2()
+    }
+    if (document.getElementById('option-17').checked) {
+      document.getElementById('block-17').style.border = '3px solid red'
+      document.getElementById('result-17').style.color = 'red'
+      document.getElementById('result-17').innerHTML = 'Incorrect!'
+      showCorrectAnswer2()
+    }
+    if (document.getElementById('option-18').checked) {
+      document.getElementById('block-18').style.border = '3px solid red'
+      document.getElementById('result-18').style.color = 'red'
+      document.getElementById('result-18').innerHTML = 'Correct!'
+      showCorrectAnswer2()
+    }
+  }
+  // functionen viser linket det viser det rigtige svar ved forkert svar
+  function showCorrectAnswer2() {
+    let showAnswer2 = document.createElement('p')
+    showAnswer2.innerHTML = 'Show Corrent Answer' // Nedenstående er innerstyling i javascript på elementet p^
+    showAnswer2.style.fontSize = '1.75rem'
+    showAnswer2.style.color = 'white'
+    showAnswer2.style.display = 'flex'
+    showAnswer2.style.justifyContent = 'center'
+    document.getElementById('showanswer2').appendChild(showAnswer2)
+    showAnswer2.addEventListener('click', () => {
+      document.getElementById('block-18').style.border = '3px solid limegreen'
+      document.getElementById('result-18').style.color = 'limegreen'
+      document.getElementById('result-18').innerHTML = 'Correct!'
+      document.getElementById('showanswer2').removeChild(showAnswer2)
     })
   }
